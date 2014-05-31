fa2694af938d3e3eea7f89a262cd97fc17774e53
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-05-31 07:39:17 -0700

    Extract ReactTest helper, clean up example react.js test

--- src/test/js/LabelSpec.js
+++ src/test/js/LabelSpec.js
@@ -1,50 +1,29 @@
 /** @jsx React.DOM */
 
-"use strict";
-
-var React,
-  ReactTestUtils;
+var ReactTest = require('./ReactTest');
+var expect = require('chai').expect;
 var Label;
-var assert = require("assert");
-var jsdom = require('jsdom').jsdom;
-
-global.initDOM = function() {
-  var jsdom = require('jsdom');
-  global.window = jsdom.jsdom().createWindow('<html><body></body></html>');
-  global.document = window.document;
-  global.navigator = window.navigator;
-
-  React = require("react/addons"),
-  ReactTestUtils = React.addons.TestUtils;
-  Label = require("../../main/js/Label");
-
-}
-
-global.cleanDOM = function() {
-  delete global.window;
-  delete global.document;
-  delete global.navigator;
-}
 
 describe("Label Test", function() {
-
   beforeEach(function() {
-    initDOM();
+    ReactTest.initDOM();
+    Label = require("../../main/js/Label");
   });
 
   afterEach(function() {
-    cleanDOM();
+    ReactTest.cleanDOM();
   });
 
   it("Check Text Assignment", function() {
-    var label = ReactTestUtils.renderIntoDocument( <Label>Some Text We Need for Test</Label>);
-      assert.equal(label.refs.p.props.children, "Some Text We Need for Test");
-    });
+    var label = ReactTest.render(<Label>Some Text We Need
+      for Test</Label>);
+    expect(label.refs.p.props.children).to.equal("Some Text We Need for Test");
+  });
 
-    it("Click", function () {
-      var label = ReactTestUtils.renderIntoDocument(<Label>Some Text We Need to Test</Label > );
+  it("Click", function () {
+    var label = ReactTest.render(<Label>Some Text We Need to Test</Label>);
 
-    ReactTestUtils.Simulate.click(label.refs.p.getDOMNode());
-    assert.equal(label.refs.p.props.children, "Text After Click");
+    ReactTest.click(label.refs.p);
+    expect(label.refs.p.props.children).to.equal("Text After Click");
   });
 });
---
+++ src/test/js/ReactTest.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var React, ReactTestUtils;
+var assert = require("assert");
+var jsdom = require('jsdom').jsdom;
+
+exports.initDOM = function() {
+  var jsdom = require('jsdom');
+  global.window = jsdom.jsdom().createWindow('<html><body></body></html>');
+  global.document = window.document;
+  global.navigator = window.navigator;
+
+  React = require("react/addons"),
+  ReactTestUtils = React.addons.TestUtils;
+}
+
+exports.cleanDOM = function() {
+  delete global.window;
+  delete global.document;
+  delete global.navigator;
+  React = undefined;
+  ReactTestUtils = undefined;
+}
+
+exports.render = function(component) {
+  return ReactTestUtils.renderIntoDocument(component);
+}
+
+exports.click = function(component) {
+  ReactTestUtils.Simulate.click(component.getDOMNode());
+}
