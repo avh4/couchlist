710972d1d6b36e54f8eb78fcc9e95e27b4214ad8
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-06-01 12:12:50 -0700

    Extract ReactTest.mock from {{project}}Spec

--- src/test/js/ReactTest.js
+++ src/test/js/ReactTest.js
@@ -7,3 +7,12 @@ var cheerio = require('cheerio');
 exports.render = function(component) {
   return cheerio.load(React.renderComponentToString(component));
 }
+
+exports.mock = function(subject, componentNameToMock) {
+  var mock = React.createClass({
+    render: function() {
+      return React.DOM.div({id: componentNameToMock}, JSON.stringify(this.props));
+    }
+  });
+  subject.__set__(componentNameToMock, mock);
+}
