fe8216af74363fe6cc78f3cfa527d2f33d1fac07
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-06-01 12:16:05 -0700

    Throw when ReactTest.mock is given a subject that is not loaded with rewire

--- src/test/js/ReactTest.js
+++ src/test/js/ReactTest.js
@@ -9,6 +9,9 @@ exports.render = function(component) {
 }
 
 exports.mock = function(subject, componentNameToMock) {
+  if (typeof subject.__set__ != 'function') {
+    throw new Error('ReactTest.mock subjects must use rewire');
+  }
   var mock = React.createClass({
     render: function() {
       return React.DOM.div({id: componentNameToMock}, JSON.stringify(this.props));
