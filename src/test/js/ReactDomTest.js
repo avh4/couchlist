f354c05244575d8cbd76d64a56a039272adcc5dd
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-06-01 21:57:27 -0700

    Replace cheerio tests with jsdom tests

--- src/test/js/ReactDomTest.js
+++ src/test/js/ReactDomTest.js
@@ -36,3 +36,19 @@ exports.input = function(selector, value) {
   var target = $(selector).get(0);
   ReactTestUtils.Simulate.change(target, { target: { value: value }});
 }
+
+exports.mock = function(subject, componentNameToMock) {
+  if (typeof subject.__set__ != 'function') {
+    throw new Error('ReactTest.mock subjects must use rewire');
+  }
+  var mock = React.createClass({
+    render: function() {
+      return React.DOM.div({id: componentNameToMock}, JSON.stringify(this.props));
+    }
+  });
+  subject.__set__(componentNameToMock, mock);
+}
+
+exports.mock.props = function(element) {
+  return JSON.parse(element.text());
+}
