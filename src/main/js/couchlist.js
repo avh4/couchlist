1b0203c8f4a1d86ce9430b4b9bee6348c1b0c7f6
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-07-10 20:56:20 -0700

    Listen to CouchDB for changes and refresh the list

--- src/main/js/couchlist.js
+++ src/main/js/couchlist.js
@@ -7,15 +7,33 @@ var TheList = require('./TheList');
 
 var request = require('browser-request');
 
+var since = 0;
 
+function poll(callback) {
+  console.log('Polling: ' + since);
+  request('http://localhost:5984/couchlist-dev/_changes?feed=longpoll&since=' + since,
+    function(err, res, body) {
+      var b = JSON.parse(body);
+      since = b.last_seq;
+      callback();
+      setTimeout(poll.bind(null, callback));
+    });
+}
 
 module.exports = React.createClass({
   getInitialState: function() {
-    return { items: [] };
+    return {
+      items: []
+    };
   },
   componentDidMount: function() {
-    request('http://localhost:5984/couchlist-dev/_all_docs?include_docs=true', function(err, res, body) {
-      this.setState({items: JSON.parse(body).rows});
+    poll(function() {
+      request('http://localhost:5984/couchlist-dev/_all_docs?include_docs=true',
+        function(err, res, body) {
+          this.setState({
+            items: JSON.parse(body).rows
+          });
+        }.bind(this));
     }.bind(this));
   },
   render: function() {
