6c6a7298c5ffd03cc5eb64d28c51c38739394af7
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-07-10 20:18:35 -0700

    Add placeholder icon to the list

--- src/main/js/TheList.js
+++ src/main/js/TheList.js
@@ -7,7 +7,7 @@ var React = require('react');
 module.exports = React.createClass({
   render: function() {
     return <ul className="list-group">
-    <li className="list-group-item">Cras justo odio</li>
+    <li className="list-group-item"><span className="glyphicon glyphicon-star"></span>Cras justo odio</li>
     <li className="list-group-item">Dapibus ac facilisis in</li>
     <li className="list-group-item">Morbi leo risus</li>
     <li className="list-group-item">Porta ac consectetur ac</li>
